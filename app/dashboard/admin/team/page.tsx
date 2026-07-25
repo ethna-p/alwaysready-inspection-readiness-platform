dc2c7e666940489cfa23f2c9b488d15013d6d46e
AJ Parker <ethnainiceland@gmail.com>
2026-07-25 21:01:02 +0100

    Delete redundant admin/team folder

--- app/dashboard/admin/team/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-
-// Team management has moved to the Account settings page.
-export default function TeamPageRedirect() {
-  redirect('/dashboard/account')
-}
